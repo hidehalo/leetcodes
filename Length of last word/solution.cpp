40cc33130e11bea58952cf7cbec07077a0d554bb
hidehalo <tianchen_cc@yeah.net>
2017-05-03 16:30:32 +0800

    init

---
+++ Length of last word/solution.cpp
@@ -0,0 +1,11 @@
+int lengthOfLastWord(const char* s) {
+        int len = 0;
+        while (*s) {
+            if (*s++ != ' ')
+                ++len;
+            else if (*s && *s != ' ')
+                len = 0;
+    
+        }
+        return len;
+}
