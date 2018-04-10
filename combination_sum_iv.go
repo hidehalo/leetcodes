b208297ae287be0e355a323ca4b81d7ad45ef38c
tianchen <tianchen@g-i.asia>
2018-04-10 12:29:33 +0800

    dp solution

--- combination_sum_iv.go
+++ combination_sum_iv.go
@@ -4,24 +4,30 @@ import (
 	"fmt"
 )
 
+var dp []int
+
 // Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
 func combinationSum4(nums []int, target int) int {
+	dp = make([]int, target+1)
+	for i := range dp {
+		dp[i] = -1
+	}
+	dp[0] = 1
+
+	return reach(&nums, target)
+}
+
+func reach(nums *[]int, target int) int {
+	if dp[target] != -1 {
+		return dp[target]
+	}
 	cnt := 0
-	for _, v := range nums {
-		next := target - v
-		if next < 0 {
-			continue
-		}
-		if next == 0 {
-			cnt++
-		}
-		for _, tmp := range nums {
-			if tmp <= next {
-				cnt += combinationSum4(nums, next)
-				break
-			}
+	for _, v := range *nums {
+		if target >= v {
+			cnt += reach(nums, target-v)
 		}
 	}
+	dp[target] = cnt
 
 	return cnt
 }
