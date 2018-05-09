fda25bd8e84adf2ebff9039888465876f9f9e0fc
hidehalo <tianchen_cc@yeah.net>
2018-05-09 23:26:03 +0800

    add symmetirc tree impls

---
+++ Symmetric_Tree.go
@@ -0,0 +1,14 @@
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+}
