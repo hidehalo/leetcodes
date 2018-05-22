0f08a4a76931143419bfd7562193b9ae9a4bc53b
tianchen <tianchen@g-i.asia>
2018-05-22 15:43:09 +0800

    unsolved: add impl of word break

---
+++ Word_Break.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func wordBreak(s string, wordDict []string) bool {
+	for _, word := range wordDict {
+		p := strings.Index(s, word)
+		if p >= 0 {
+			s = s[:p] + s[len(word)+p:]
+		} else {
+			return false
+		}
+	}
+	if len(s) > 0 {
+		for _, str := range wordDict {
+			if str == s {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(wordBreak("applepenapple", []string{"pena", "apple"}))
+}
