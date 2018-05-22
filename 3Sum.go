79a58ca26d8c79d6dafef60adc2890139f56cf0c
hidehalo <tianchen_cc@yeah.net>
2018-05-22 21:45:39 +0800

    timeout: update of three sum

--- 3Sum.go
+++ 3Sum.go
@@ -13,7 +13,7 @@ func threeSum(nums []int) [][]int {
 	i := 0
 	j := i + 1
 	k := j + 1
-	hashSet := make(map[int]bool)
+	hashSet := make([]map[int]int, 0, size)
 	for i < size-3 {
 		if k > size-1 {
 			j++
@@ -23,21 +23,29 @@ func threeSum(nums []int) [][]int {
 			}
 			k = j + 1
 		}
-		fmt.Println("nums:", nums[i], nums[j], nums[k])
-		fmt.Println("hash set:", hashSet[nums[i]], hashSet[nums[j]], hashSet[nums[k]])
-		if hashSet[nums[i]] && hashSet[nums[j]] && hashSet[nums[k]] {
+		var dup bool
+		for _, set := range hashSet {
+			tmp := make(map[int]int)
+			tmp[nums[i]]++
+			tmp[nums[j]]++
+			tmp[nums[k]]++
+			if tmp[nums[i]] == set[nums[i]] && tmp[nums[j]] == set[nums[j]] && tmp[nums[k]] == set[nums[k]] {
+				dup = true
+				break
+			}
+		}
+		if dup {
 			j++
 			k = j + 1
 			continue
 		}
-		// fmt.Println("index:", i, j, k)
 		if nums[i]+nums[j]+nums[k] == 0 {
-			if !hashSet[nums[i]] || !hashSet[nums[j]] || !hashSet[nums[k]] {
-				ret = append(ret, []int{nums[i], nums[j], nums[k]})
-				hashSet[nums[i]] = true
-				hashSet[nums[j]] = true
-				hashSet[nums[k]] = true
-			}
+			ret = append(ret, []int{nums[i], nums[j], nums[k]})
+			set := make(map[int]int)
+			set[nums[i]]++
+			set[nums[j]]++
+			set[nums[k]]++
+			hashSet = append(hashSet, set)
 		}
 		k++
 	}
@@ -46,5 +54,5 @@ func threeSum(nums []int) [][]int {
 }
 
 func main() {
-	fmt.Println(threeSum([]int{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}))
+	fmt.Println(threeSum([]int{-13, 5, 13, 12, -2, -11, -1, 12, -3, 0, -3, -7, -7, -5, -3, -15, -2, 14, 14, 13, 6, -11, -11, 5, -15, -14, 5, -5, -2, 0, 3, -8, -10, -7, 11, -5, -10, -5, -7, -6, 2, 5, 3, 2, 7, 7, 3, -10, -2, 2, -12, -11, -1, 14, 10, -9, -15, -8, -7, -9, 7, 3, -2, 5, 11, -13, -15, 8, -3, -7, -12, 7, 5, -2, -6, -3, -10, 4, 2, -5, 14, -3, -1, -10, -3, -14, -4, -3, -7, -4, 3, 8, 14, 9, -2, 10, 11, -10, -4, -15, -9, -1, -1, 3, 4, 1, 8, 1}))
 }
