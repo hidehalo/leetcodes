c0ea13e960c8685d49e6d4172011b409373f59c2
hidehalo <tianchen_cc@yeah.net>
2018-06-01 16:34:09 +0800

    solved: add impl of interger of roman

--- Integer_to_Roman.go
+++ Integer_to_Roman.go
@@ -1,9 +1,55 @@
 package main
 
-func intToRoman(num int) string {
+import (
+	"fmt"
+)
 
+func intToRoman(num int) string {
+	table := map[int]string{
+		1000: "M",
+		900:  "CM",
+		500:  "D",
+		400:  "CD",
+		100:  "C",
+		90:   "XC",
+		50:   "L",
+		40:   "XL",
+		10:   "X",
+		9:    "IX",
+		5:    "V",
+		4:    "IV",
+		1:    "I",
+	}
+	keys := []int{
+		1000,
+		900,
+		500,
+		400,
+		100,
+		90,
+		50,
+		40,
+		10,
+		9,
+		5,
+		4,
+		1,
+	}
+	ret := ""
+	i := 0
+	for num > 0 {
+		v := keys[i]
+		if num >= v {
+			num -= v
+			ret += table[v]
+		} else {
+			i++
+		}
+	}
+
+	return ret
 }
 
 func main() {
-
+	fmt.Println(intToRoman(1994))
 }
