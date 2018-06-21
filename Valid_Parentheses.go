0dce5b766371555cc50369b36f7d8740780ba84f
hidehalo <tianchen_cc@yeah.net>
2018-06-22 00:28:09 +0800

    solved: add vaild parentheses

---
+++ Valid_Parentheses.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Stack struct {
+	Top   int
+	store []byte
+}
+
+func NewStack() *Stack {
+	return &Stack{
+		Top:   -1,
+		store: make([]byte, 0, 1000),
+	}
+}
+
+func (s *Stack) Pop() byte {
+	if s.Top < 0 {
+		return 0
+	}
+	ret := s.store[s.Top]
+	s.Top--
+
+	return ret
+}
+
+func (s *Stack) Push(e byte) {
+	s.Top++
+	if len(s.store) <= s.Top {
+		s.store = append(s.store, e)
+	} else {
+		s.store[s.Top] = e
+	}
+}
+
+func isValid(s string) bool {
+	stack := NewStack()
+	pattern := map[byte]byte{
+		'{': '}',
+		'[': ']',
+		'(': ')',
+	}
+	size := len(s)
+	for i := 0; i < size; i++ {
+		if s[i] == '{' || s[i] == '[' || s[i] == '(' {
+			stack.Push(s[i])
+		} else if s[i] == '}' || s[i] == ']' || s[i] == ')' {
+			if stack.Top < 0 {
+				return false
+			} else if pattern[stack.Pop()] != s[i] {
+				return false
+			}
+		}
+	}
+
+	return stack.Top < 0
+}
+
+func main() {
+	fmt.Println(isValid("()[]{}"))
+}
