1ea8599bcd1304f270460226be810db29daa1c0c
hidehalo <tianchen_cc@yeah.net>
2018-07-25 15:04:42 +0800

    solved: update impl of strStr

--- Implement_strStr.go
+++ Implement_strStr.go
@@ -8,30 +8,46 @@ func strStr(haystack string, needle string) int {
 	if needle == "" {
 		return 0
 	}
-	var i, j, ret int
-	size := len(haystack)
-	sizeP := len(needle)
-	for i = 0; i < size; i++ {
-		if j >= sizeP {
+	for i, c := range haystack {
+		if i+len(needle) > len(haystack) {
 			break
-		} else if j > 0 && haystack[i] != needle[j] {
-			j = 0
-			i = ret
+		} else if byte(c) == needle[0] && haystack[i:i+len(needle)] == needle {
+			return i
 		}
-		if haystack[i] == needle[0] {
-			ret = i
-		}
-		if haystack[i] == needle[j] {
-			j++
-		}
-	}
-	if j == sizeP {
-		return ret
 	}
 
 	return -1
+	// hSize := len(haystack)
+	// nSize := len(needle)
+	// offsetIndecies := make([]int, 0, hSize)
+	// for i, c := range haystack {
+	// 	if byte(c) == needle[0] && i+nSize <= hSize {
+	// 		offsetIndecies = append(offsetIndecies, i)
+	// 	}
+	// }
+	// ret := -1
+	// if len(offsetIndecies) > 0 {
+	// 	for _, offset := range offsetIndecies {
+	// 		ret = conquer(haystack, offset, needle)
+	// 		if ret >= 0 {
+	// 			break
+	// 		}
+	// 	}
+	// }
+
+	// return ret
 }
 
+// func conquer(haystack string, offset int, needle string) int {
+// 	for i, c := range needle {
+// 		if haystack[offset+i] != byte(c) {
+// 			return -1
+// 		}
+// 	}
+
+// 	return offset
+// }
+
 func main() {
-	fmt.Println(strStr("mississippi", "issip"))
+	fmt.Println(strStr("hello", "ll"))
 }
