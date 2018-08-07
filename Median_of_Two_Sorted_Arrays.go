3a13c6f4e91bdd345e302eb03ddd5237d58988fa
hidehalo <tianchen_cc@yeah.net>
2018-08-07 23:08:34 +0800

    unsolved: can not get that median of a smaller even set(both even)

--- Median_of_Two_Sorted_Arrays.go
+++ Median_of_Two_Sorted_Arrays.go
@@ -8,14 +8,24 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	size1 := len(nums1)
 	size2 := len(nums2)
 	stopIndex := (size1 + size2) / 2
-	if size1 <= 0 {
-		return float64(nums2[stopIndex])
-	} else if size2 <= 0 {
-		return float64(nums1[stopIndex])
-	}
 	var isEven bool
 	if (size1+size2)%2 == 0 {
 		isEven = true
+	}
+	if size1 <= 0 && size2 > 0 {
+		if isEven == true {
+			return 0.5 * float64(nums2[stopIndex-1]+nums2[stopIndex])
+		}
+
+		return float64(nums2[stopIndex])
+	} else if size2 <= 0 && size1 > 0 {
+		if isEven == true {
+			return 0.5 * float64(nums1[stopIndex-1]+nums1[stopIndex])
+		}
+
+		return float64(nums1[stopIndex])
+	} else if size1 <= 0 && size2 <= 0 {
+		return 0.0
 	}
 	p1 := 0
 	p2 := 0
@@ -29,13 +39,13 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 			p1++
 		}
 	}
+	if p2 >= size2 {
+		p2 = size2 - 1
+	}
+	if p1 >= size1 {
+		p1 = size1 - 1
+	}
 	if isEven == true {
-		if p2 >= size2 {
-			p2 = size2 - 1
-		}
-		if p1 >= size1 {
-			p1 = size1 - 1
-		}
 
 		return 0.5 * float64(nums1[p1]+nums2[p2])
 	}
@@ -48,6 +58,6 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
 func main() {
 	nums1 := []int{1, 2}
-	nums2 := []int{}
+	nums2 := []int{-1, 3}
 	fmt.Println(findMedianSortedArrays(nums1, nums2))
 }
