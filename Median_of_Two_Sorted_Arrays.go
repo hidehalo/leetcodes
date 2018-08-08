848069bd5adaf907c49e9471f0bd1d9c50ccc1d7
hidehalo <tianchen_cc@yeah.net>
2018-08-08 14:25:45 +0800

    solved: make algo more correct&efficent

--- Median_of_Two_Sorted_Arrays.go
+++ Median_of_Two_Sorted_Arrays.go
@@ -4,60 +4,81 @@ import (
 	"fmt"
 )
 
+type Pointer struct {
+	Arr   *[]int
+	Index int
+	Tail  int
+}
+
+func (p *Pointer) Value() int {
+	if p.Index > p.Tail {
+		return 2 << (32 - 2)
+	}
+
+	return (*p.Arr)[p.Index]
+}
+
+func rankValueOfTwoSortedArray(nums1 []int, nums2 []int, rank int) float64 {
+	size1 := len(nums1)
+	size2 := len(nums2)
+	p1 := &Pointer{&nums1, 0, size1 - 1}
+	p2 := &Pointer{&nums2, 0, size2 - 1}
+	ret := 0.0
+	if rank == 1 {
+		if size1 == 0 {
+			ret = float64(p2.Value())
+		} else if size2 == 0 {
+			ret = float64(p1.Value())
+		} else if size1 != 0 && size2 != 0 {
+			if p1.Value() > p2.Value() {
+				ret = float64(p2.Value())
+			} else {
+				ret = float64(p1.Value())
+			}
+		}
+
+		return ret
+	}
+	for rank-1 > 0 {
+		rank--
+		if p1.Value() > p2.Value() {
+			tmp := p1
+			p1 = p2
+			p2 = tmp
+		}
+		p1.Index++
+		if p1.Value() < p2.Value() {
+			ret = float64(p1.Value())
+		} else {
+			ret = float64(p2.Value())
+		}
+	}
+
+	return ret
+}
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	size1 := len(nums1)
 	size2 := len(nums2)
-	stopIndex := (size1 + size2) / 2
-	var isEven bool
-	if (size1+size2)%2 == 0 {
-		isEven = true
-	}
-	if size1 <= 0 && size2 > 0 {
-		if isEven == true {
-			return 0.5 * float64(nums2[stopIndex-1]+nums2[stopIndex])
-		}
-
-		return float64(nums2[stopIndex])
-	} else if size2 <= 0 && size1 > 0 {
-		if isEven == true {
-			return 0.5 * float64(nums1[stopIndex-1]+nums1[stopIndex])
-		}
-
-		return float64(nums1[stopIndex])
-	} else if size1 <= 0 && size2 <= 0 {
-		return 0.0
-	}
-	p1 := 0
-	p2 := 0
-	for p1 < size1 && p2 < size2 {
-		if p1+p2 >= stopIndex {
-			break
-		}
-		if nums1[p1] > nums2[p2] {
-			p2++
-		} else {
-			p1++
-		}
-	}
-	if p2 >= size2 {
-		p2 = size2 - 1
-	}
-	if p1 >= size1 {
-		p1 = size1 - 1
-	}
+	rank := (size1 + size2 + 1) / 2
+	isEven := ((size1+size2)%2 == 0)
+	medians := make([]float64, 2)
+	k := 1.0
+	medians[0] = rankValueOfTwoSortedArray(nums1, nums2, rank)
 	if isEven == true {
-
-		return 0.5 * float64(nums1[p1]+nums2[p2])
+		k = 0.5
+		medians[1] = rankValueOfTwoSortedArray(nums1, nums2, rank+1)
 	}
-	if nums1[p1] < nums2[p2] {
-		return float64(nums1[p1])
+	var sum float64
+	for _, v := range medians {
+		sum += v
 	}
 
-	return float64(nums2[p2])
+	return k * sum
 }
 
 func main() {
-	nums1 := []int{1, 2}
-	nums2 := []int{-1, 3}
+	nums1 := []int{1}
+	nums2 := []int{1}
 	fmt.Println(findMedianSortedArrays(nums1, nums2))
 }
