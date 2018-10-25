ad57f15158132006cc43be07045a9af7a8f372d3
hidehalo <tianchen_cc@yeah.net>
2018-10-25 18:44:49 +0800

    Update: heap.go heap adt&min&heap max heap impls

--- ds/heap.go
+++ ds/heap.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
 type key int
-type val uint
+type val int
 type pri int
 
 const (
@@ -10,9 +16,26 @@ const (
 	Priority_LO pri = 2
 )
 
+type IHeap interface {
+	Compare(a val, b val) pri
+	Top() val
+	Insert(v val)
+	ExtractTop()
+	String()
+}
+
 type Heap struct {
+	IHeap
 	size  uint
 	store []val
+}
+
+type MinHeap struct {
+	Heap
+}
+
+type MaxHeap struct {
+	Heap
 }
 
 func (h *Heap) Top() val {
@@ -20,19 +43,28 @@ func (h *Heap) Top() val {
 }
 
 func (h *Heap) Insert(v val) {
+	h.store = append(h.store, v)
 	h.size++
-	h.store[h.size-1] = v
+	i := key(h.size - 1)
+	fmt.Println(*h)
+	isHi := h.Compare(h.store[h.parent(i)], h.store[i]) == Priority_HI
+	for i > 0 && !isHi {
+		h.swap(h.parent(i), i)
+		i = h.parent(i)
+	}
 }
 
 func (h *Heap) ExtractTop() val {
-	// bottom to top
-	// size--
-	// heapify
-	return h.store[0]
-}
+	if h.size >= 1 {
+		top := h.store[0]
+		h.store[0] = h.store[h.size-1]
+		h.size--
+		h.heapify(0)
 
-func (h *Heap) Compare(a val, b val) pri {
-	return Priority_EQ
+		return top
+	}
+
+	return h.store[0]
 }
 
 func (h *Heap) heapify(hi key) {
@@ -63,12 +95,76 @@ func (h *Heap) right(i key) key {
 	return h.left(i) + 1
 }
 
+func (h *Heap) String() string {
+	var strBuf bytes.Buffer
+	i := 0
+	for i < int(h.size) {
+		strBuf.WriteString(strconv.FormatInt(int64(h.store[i]), 32))
+		i++
+	}
+
+	return strBuf.String()
+}
+
 func (h *Heap) swap(a key, b key) {
 	tmp := h.store[a]
 	h.store[a] = h.store[b]
 	h.store[b] = tmp
 }
 
-func main() {
+func (h *Heap) initHeap(vals *[]val) {
+	for _, v := range *vals {
+		h.Insert(v)
+	}
+}
 
+func NewHeap(vals []val) Heap {
+	h := Heap{
+		size:  0,
+		store: make([]val, 0, 1000),
+	}
+
+	return h
+}
+
+func NewMinHeap(vals []val) *MinHeap {
+	h := &MinHeap{
+		Heap: NewHeap(vals),
+	}
+	h.initHeap(&vals)
+
+	return h
+}
+
+func NewMaxHeap(vals []val) *MaxHeap {
+	h := &MaxHeap{
+		Heap: NewHeap(vals),
+	}
+	h.initHeap(&vals)
+
+	return h
+}
+
+func (h *MinHeap) Compare(a val, b val) pri {
+	if a < b {
+		return Priority_HI
+	}
+
+	return Priority_LO
+}
+
+func (h *MaxHeap) Compare(a val, b val) pri {
+	if a > b {
+		return Priority_HI
+	}
+
+	return Priority_LO
+}
+
+func main() {
+	vals := []val{1, 2, 3, 4, 5}
+	maxHeap := NewMaxHeap(vals)
+	minHeap := NewMinHeap(vals)
+	fmt.Println(maxHeap)
+	fmt.Println(minHeap)
 }
