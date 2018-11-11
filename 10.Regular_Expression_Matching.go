de97fbb2d665fd5db5661fb4c77e361bf43e157f
hidehalo <tianchen_cc@yeah.net>
2018-11-11 23:35:34 +0800

    unsolved: update new thoughts

--- 10.Regular_Expression_Matching.go
+++ 10.Regular_Expression_Matching.go
@@ -2,6 +2,54 @@ package main
 
 import "fmt"
 
+type DoubleLinkListNode struct {
+	Prev *DoubleLinkListNode
+	Next *DoubleLinkListNode
+	val  byte
+}
+
+func (node *DoubleLinkListNode) Val() byte {
+	return node.val
+}
+
+type Stack struct {
+	store *DoubleLinkListNode
+	tail  *DoubleLinkListNode
+	len   int
+}
+
+func (s *Stack) Push(val byte) {
+	s.tail.Next = &DoubleLinkListNode{
+		s.tail,
+		nil,
+		val,
+	}
+	s.tail = s.tail.Next
+	s.len++
+}
+
+func (s *Stack) Pop() *DoubleLinkListNode {
+	node := s.tail
+	s.tail = s.tail.Prev
+	s.len--
+
+	return node
+}
+
+func (s *Stack) Len() int {
+	return s.len
+}
+
+func NewStack() *Stack {
+	head := &DoubleLinkListNode{}
+
+	return &Stack{
+		head,
+		head,
+		0,
+	}
+}
+
 func isMatch(s string, p string) bool {
 	if p == ".*" {
 		return true
@@ -17,9 +65,10 @@ func isMatch(s string, p string) bool {
 		return false
 	}
 	i, j := 0, 0
-	var cache byte
+	stack := NewStack()
 	for i < sizeS && j < sizeP {
 		if p[j] == '.' {
+			// pattern "."
 			if j < sizeP-1 && p[j+1] == '*' {
 				return true
 			}
@@ -27,44 +76,54 @@ func isMatch(s string, p string) bool {
 			i++
 			continue
 		} else if j < sizeP-1 && p[j+1] == '*' {
-			// for i < sizeS && s[i] == p[j] {
-			// 	i++
-			// }
-			// j += 2
-			// if i >= sizeS && j < sizeP {
-			// 	fmt.Println(sizeP-j, i)
-			// 	if sizeP-j > i {
-			// 		return false
-			// 	}
-			// 	for j < sizeP && s[sizeS-1] == p[j] {
-			// 		j++
-			// 	}
-			// }
-			for i < sizeS && s[i] == cache {
-				i++
-			}
-			cache = p[j]
-			if s[i] == p[j] {
-				i++
-			}
+			// pattern "char*"
+			// use stack impl lazy match
+			stack.Push(p[j])
 			j += 2
 			continue
-		}
-		if s[i] != p[j] {
-			if s[i] == cache {
+		} else {
+			// pattern ascii char
+			if p[j] == s[i] {
 				i++
+				j++
+			} else if p[j] != s[i] && stack.Len() > 0 {
+				for stack.Len() > 0 {
+					node := stack.Pop()
+					if node.Val() == s[i] {
+						i++
+						break
+					}
+				}
 			} else {
 				return false
 			}
-		} else {
-			i++
-			j++
 		}
 	}
+	fmt.Println(stack.Len(), i, j)
+	// FIXME: it can not do backtracing match when alphabet pattern is longer
+	if i < sizeS {
+		return false
+	}
+	switch sizeP - j {
+	case 0:
+		return true
+	case 1:
+		if p[j] != '*' {
+			return false
+		}
+		break
+	case 2:
+		if !(p[j] == '.' && p[j+1] == '*') {
+			return false
+		}
+		break
+	default:
+		return false
+	}
 
-	return true
+	return false
 }
 
 func main() {
-	fmt.Println(isMatch("aaa", "a*aaaaaaa"))
+	fmt.Println(isMatch("aaa", "a*aa.*"))
 }
