1fc63cce12f4b66486359cd253da7c0237044f34
hidehalo <tianchen_cc@yeah.net>
2018-12-02 20:52:27 +0800

    unsolved: update combination_sum2

--- 40.Combination_Sum _II.go
+++ 40.Combination_Sum _II.go
@@ -1,31 +1,39 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+)
 
 func combinationSum2(candidates []int, target int) [][]int {
-	sort.Ints(candidates)
 	size := len(candidates)
-	start, end := 0, size-1
 	ret := make([][]int, 0, size)
-	for start < end {
-		if candidates[start]+candidates[end] > target {
-			end--
-		} else if candidates[start]+candidates[end] < target {
-			start++
-		} else {
-			ret = append(ret, []int{start, target})
-			for candidates[start] == candidates[start+1] && start < end {
-				start++
-			}
-			for candidates[end] == candidates[end-1] && start < end {
-				end--
-			}
-			start++
-			end--
-		}
+	recur(candidates, target)
+
+	return ret
+}
+
+func recur(candidates []int, target int) {
+	size := len(candidates)
+	fmt.Println(size, target)
+	if size <= 0 {
+		return
+	}
+	if target == candidates[0] {
+		// fmt.Println(candidates[0], "fi")
+	}
+	if target < candidates[0] {
+		// fmt.Println("no")
+	}
+	if target > candidates[0] {
+		// fmt.Println(candidates[0])
+	}
+	for i := 0; i < size; i++ {
+		recur(candidates[1:], target-candidates[0])
 	}
 }
 
 func main() {
-
+	candidates := []int{2, 5, 2, 1, 2}
+	target := 5
+	combinationSum2(candidates, target)
 }
