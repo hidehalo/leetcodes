87a36bdb25d360494debde3678417d083d6cb553
田晨 <tianchen@iMac03010130.local>
2018-12-11 18:57:41 +0800

    add: alg.sort files

---
+++ alg/sort/merge_sort.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func combine(l []int, r []int) []int {
+	var i, j int
+	a := make([]int, 0, len(l)+len(r))
+	for i < len(l) || j < len(r) {
+		if i >= len(l) {
+			a = append(a, r[j])
+			j++
+		} else if j >= len(r) {
+			a = append(a, l[i])
+			i++
+		} else if l[i] > r[j] {
+			a = append(a, r[j])
+			j++
+		} else {
+			a = append(a, l[i])
+			i++
+		}
+	}
+
+	return a
+}
+
+func merge_sort(a []int, l int, r int) []int {
+	if l == r {
+		return a
+	}
+	mid := (l + r) / 2
+	fmt.Println(l, mid, r)
+	la := merge_sort(a, l, mid)
+	ra := merge_sort(a, mid+1, r)
+	return combine(la, ra)
+}
+
+func main() {
+	nums := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	fmt.Println(merge_sort(nums, 0, len(nums)-1))
+}
