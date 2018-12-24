7bd17d86fc52b5a3b2c647761a4e4eb10f4d0052
hidehalo <tianchen_cc@yeah.net>
2018-12-24 21:30:30 +0800

    solved: post for multiply string problem

--- 43.Multiply_Strings.go
+++ 43.Multiply_Strings.go
@@ -6,28 +6,31 @@ func multiply(num1 string, num2 string) string {
 	if num1 == "0" || num2 == "0" {
 		return "0"
 	}
-	buffer := make([]byte, len(num1)+len(num2))
+	maxSize := len(num1) + len(num2)
+	buffer := make([]byte, maxSize)
 	for k := 0; k < len(buffer); k++ {
 		buffer[k] = '0'
 	}
 	round := 0
 	for i := len(num1) - 1; i >= 0; i-- {
-		offset := len(buffer) - 1 - round
+		offset := maxSize - 1 - round
 		carray := 0
 		addCarray := 0
 		char := byte('0')
 		for j := len(num2) - 1; j >= 0; j-- {
-			fmt.Printf("执行%c * %c + %d ", num1[i], num2[j], carray)
+			// fmt.Printf("执行%c * %c + %d ", num1[i], num2[j], carray)
 			char, carray = product(num1[i], num2[j], carray)
-			fmt.Printf("= %c 进 %d\n", char, carray)
-			fmt.Printf("执行%c + %c + %d ", buffer[offset], char, addCarray)
+			// fmt.Printf("= %c 进 %d\n", char, carray)
+			// fmt.Printf("执行%c + %c + %d ", buffer[offset], char, addCarray)
 			buffer[offset], addCarray = add(buffer[offset], char, addCarray)
-			fmt.Printf("= %c 进 %d\n", buffer[offset], addCarray)
+			// fmt.Printf("= %c 进 %d\n", buffer[offset], addCarray)
 			offset--
 		}
 		round++
 		buffer[offset] = byte(carray + addCarray + '0')
-		fmt.Println(string(buffer))
+	}
+	if buffer[0] == '0' {
+		buffer = buffer[1:]
 	}
 
 	return string(buffer)
@@ -46,7 +49,7 @@ func add(a, b byte, carry int) (byte, int) {
 }
 
 func main() {
-	num1 := "23312"
-	num2 := "7418"
+	num1 := "1192"
+	num2 := "10134"
 	fmt.Println(multiply(num1, num2))
 }
