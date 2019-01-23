389a8bc0fb9954dc140cdefb4b794c4824908296
hidehalo <tianchen_cc@yeah.net>
2019-01-23 18:51:30 +0800

    solved: check completeness of a binary tree problem

--- 958.Check_Completeness_of_a_Binary_Tree.go
+++ 958.Check_Completeness_of_a_Binary_Tree.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -15,18 +17,46 @@ type TreeNode struct {
  * }
  */
 func isCompleteTree(root *TreeNode) bool {
-	if root.Left == nil && root.Right == nil {
-		return true
-	} else {
-		return false
+	treeList := make([]*TreeNode, 0, 1000)
+	treeList = append(treeList, root)
+	i := 0
+	for i < len(treeList) && treeList[i] != nil {
+		treeList = append(treeList, treeList[i].Left)
+		treeList = append(treeList, treeList[i].Right)
+		i++
 	}
-	return isCompleteTree(root.Left) && isCompleteTree(root.Right)
+	for i < len(treeList) && treeList[i] == nil {
+		i++
+	}
+
+	return i == len(treeList)
 }
 
 func NewTree(nodes []int) *TreeNode {
+	treeList := make([]*TreeNode, 0, 1000)
+	for i := 0; i < len(nodes); i++ {
+		if nodes[i] == -1 {
+			treeList = append(treeList, nil)
+		} else {
+			treeList = append(treeList, &TreeNode{nodes[i], nil, nil})
+		}
+	}
+	for i := 0; i < len(treeList); i++ {
+		l := 2*i + 1
+		r := l + 1
+		if l < len(treeList) {
+			treeList[i].Left = treeList[l]
+		}
+		if r < len(treeList) {
+			treeList[i].Right = treeList[r]
+		}
+	}
 
+	return treeList[0]
 }
 
 func main() {
-
+	nodes := []int{1, 2, 3, 4, 5, 6, 7}
+	root := NewTree(nodes)
+	fmt.Println(isCompleteTree(root))
 }
