7759cdef851a0a6a246c423a6d350df5805696d8
hidehalo <tianchen_cc@yeah.net>
2019-04-26 17:50:33 +0800

    unsolved: update

--- 961 N-Repeated_Element_in_Size_2N_Array.go
+++ 961 N-Repeated_Element_in_Size_2N_Array.go
@@ -1,9 +1,22 @@
 package main
 
-func repeatedNTimes(A []int) int {
+import "fmt"
 
+func repeatedNTimes(A []int) int {
+	var a, b int
+	for _, num := range A {
+		b = num
+		fmt.Println(a, b)
+		if a|b-a^b == b {
+			return b
+		}
+		a = a | b
+	}
+
+	return -1
 }
 
 func main() {
-
+	num := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1}
+	fmt.Println(repeatedNTimes(num))
 }
