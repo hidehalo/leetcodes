ff7e3d5242ee42020e029f35ab6a8203a197f9d6
hidehalo <tianchen_cc@yeah.net>
2020-03-24 17:10:47 +0800

    Update: LC805 impl&test

--- 805.Split_Array_With_Same_Average/solution.go
+++ 805.Split_Array_With_Same_Average/solution.go
@@ -1,21 +1,36 @@
 package main
 
+import "sort"
+
 func splitArraySameAverage(A []int) bool {
 	sum := 0
-	for i := 0; i < len(A); i++ {
+	n := len(A)
+	for i := 0; i < n; i++ {
 		sum += A[i]
 	}
-	// TODO: impl&test
+	sort.Sort(sort.Reverse(sort.IntSlice(A)))
+	for k := 1; k <= n/2; k++ {
+		// fmt.Printf("%d*%d%%%d=%d\n", sum, k, n, sum*k%n)
+		if sum*k%n == 0 && hasKSum(A, k, sum*k/n) {
+			return true
+		}
+	}
+	return false
 }
 
 func hasKSum(A []int, k, target int) bool {
 	if target < 0 {
 		return false
+	} else if k < 0 {
+		return false
+	} else if len(A) > 0 && k*A[0] < target {
+		return false
 	} else if target == 0 && k == 0 {
 		return true
 	}
+	// fmt.Printf("A:%v,k:%d,target:%d\n", A, k, target)
 	for i := 0; i < len(A); i++ {
-		if hasKSum(A[i:], k-1, target-A[i]) {
+		if hasKSum(A[i+1:], k-1, target-A[i]) {
 			return true
 		}
 	}
--- 805.Split_Array_With_Same_Average/solution_test.go
+++ 805.Split_Array_With_Same_Average/solution_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_splitArraySameAverage(t *testing.T) {
 	type args struct {
@@ -11,12 +13,63 @@ func Test_splitArraySameAverage(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			"case1",
+			args{[]int{5, 95, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+			true,
+		},
+		{
+			"case2",
+			args{[]int{2, 12, 18, 16, 19, 3}},
+			false,
+		},
+		{
+			"case3",
+			args{[]int{3, 1, 2}},
+			true,
+		},
+		{
+			"case4",
+			args{[]int{60, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30}},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := splitArraySameAverage(tt.args.A); got != tt.want {
 				t.Errorf("splitArraySameAverage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_hasKSum(t *testing.T) {
+	type args struct {
+		A      []int
+		k      int
+		target int
+	}
+	type testCase struct {
+		name string
+		args args
+		want bool
+	}
+	tests := []testCase{
+		{
+			"[2,12,18,16,19,3]",
+			args{[]int{2, 12, 18, 16, 19, 3}, 3, 35},
+			false,
+		},
+		{
+			"[3,1,2]",
+			args{[]int{3, 1, 2}, 1, 2},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasKSum(tt.args.A, tt.args.k, tt.args.target); got != tt.want {
+				t.Errorf("hasKSum(%v) = %v, want %v", tt.name, got, tt.want)
 			}
 		})
 	}
