d1a0e27c63a5fa59a965ae49a85c8c7f2013f39a
hidehalo <tianchen_cc@yeah.net>
2020-03-24 23:59:25 +0800

    Update: LC576

--- 576.Out_of_Boundary_Paths/solution.go
+++ 576.Out_of_Boundary_Paths/solution.go
@@ -1,14 +1,26 @@
 package main
 
 func findPaths(m int, n int, N int, i int, j int) int {
-	dp := make([]int, m*n)
+	y := n - j
+	if y < 0 {
+		y = -y
+	}
+	x := m - i
+	if x < 0 {
+		x = -x
+	}
+	b := make([]int, m)
+	for k:=0;k<m;k++{
+		b[k] := make([]int,n)
+	}
+	// Set shortest path distance for boundary points
 	for k := 0; k < m; k++ {
-		dp[k] = 1
-		dp[(n-1)*m+k] = 1
+		b[0][k] = j+1
+		b[k][0]=y+k
 	}
 	for k := 0; k < n; k++ {
-		dp[k] = 1
-		dp[(m-1)*n+k] = 1
+		b[k][n] = i + 1
+		b[n][k] = x + k
 	}
 
 	// return sum of d([i,j], [boundary points]) <= N
