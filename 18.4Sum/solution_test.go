a1a5ceccc11b32ae37eb5b331d768d775d869562
hidehalo <tianchen_cc@yeah.net>
2020-04-08 15:03:32 +0800

    Update: LC18 impl&test

---
+++ 18.4Sum/solution_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fourSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"case1",
+			args{[]int{-3, -2, -1, 0, 0, 1, 2, 3}, 0},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fourSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fourSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
