80ee449893c3cd9bb1c063455f913c7f802ad653
hidehalo <tianchen_cc@yeah.net>
2020-04-14 11:03:14 +0800

    Update: LC1207 impl(solved)

--- 1207.Unique_Number_of_Occurrences/solution.go
+++ 1207.Unique_Number_of_Occurrences/solution.go
@@ -3,10 +3,14 @@ package main
 func uniqueOccurrences(arr []int) bool {
 	counts := make(map[int]int)
 	for i := 0; i < len(arr); i++ {
-		if counts[arr[i]] > 0 {
+		counts[arr[i]]++
+	}
+	occur := make(map[int]int)
+	for _, v := range counts {
+		occur[v]++
+		if occur[v] > 1 {
 			return false
 		}
-		counts[arr[i]]++
 	}
 
 	return true
