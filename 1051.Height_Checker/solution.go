61fd3e6fffde9746195f0d15c6c03390fe37826d
hidehalo <tianchen_cc@yeah.net>
2020-04-20 14:45:12 +0800

    Update: LC1051 impl(solved)

--- 1051.Height_Checker/solution.go
+++ 1051.Height_Checker/solution.go
@@ -1,24 +1,17 @@
 package main
-// import "fmt"
+
+import (
+	"sort"
+)
+
 func heightChecker(heights []int) int {
 	ret := 0
-    for i :=1;i<len(heights);i++{
-		// fmt.Printf("arr[%d]: %d, arr[%d]: %d\n", i-1, heights[i-1],i, heights[i])
-		if heights[i-1]>heights[i] {
+	sorted := make([]int, len(heights))
+	copy(sorted, heights)
+	sort.Ints(sorted)
+	for i := 0; i < len(heights); i++ {
+		if sorted[i] != heights[i] {
 			ret++
-			// find minimum & swap
-			// optimization：could use priority queue
-			min := heights[i]
-			minIndex := i
-			// fmt.Println(heights)
-			for j:=i+1;j<len(heights);j++ {
-				if heights[j] < min{
-					minIndex = j
-					min = heights[j]
-				}
-			}
-			// fmt.Printf("Swap arr[%d]:%d, arr[%d]:%d\n", i-1,heights[i-1],minIndex,heights[minIndex])
-			heights[i-1],heights[minIndex] = heights[minIndex],heights[i-1]
 		}
 	}
 	return ret
