9d5cabbf5b2ab979e7000c1319ed3e121701d976
hidehalo <tianchen_cc@yeah.net>
2020-04-21 16:49:32 +0800

    Update: LC933 impl&test(solved)

--- 933.Number_of_Recent_Calls/solution.go
+++ 933.Number_of_Recent_Calls/solution.go
@@ -1,53 +1,24 @@
 package main
 
 type RecentCounter struct {
-	head      int
-	tail      int
-	lastest   int
-	pings     []int
 	timelines []int
+	index     int
 }
 
 func Constructor() RecentCounter {
-	return RecentCounter{
-		head:      0,
-		tail:      0,
-		lastest:   0,
-		pings:     make([]int, 3000),
-		timelines: make([]int, 3000),
-	}
+	return RecentCounter{}
 }
 
 func (this *RecentCounter) Ping(t int) int {
-	startTs := t - 3000
-	if this.lastest == 0 || this.lastest != t {
-		this.lastest = t
-		this.timelines[this.tail] = t
-		this.pings[this.tail]++
-		this.tail = (this.tail + 1) % 3000
-	} else {
-		this.pings[this.tail-1]++
-	}
-	for i := 0; i < 3000; i++ {
-		if this.timelines[i] >= startTs {
-			this.head = i
-			break
-		}
-		this.pings[i] = 0
-	}
-	ret := 0
-	if this.head > this.tail {
-		for i := this.tail; i >= 0; i-- {
-			ret += this.pings[i]
-		}
-		for i := this.head; i < 3000; i++ {
-			ret += this.pings[i]
-		}
-	} else {
-		for i := this.head; i < this.tail; i++ {
-			ret += this.pings[i]
-		}
+	this.timelines = append(this.timelines, t)
+	for this.index < len(this.timelines) && this.timelines[this.index] < t-3000 {
+		this.index++
 	}
 
-	return ret
+	if this.index > 100 {
+		this.timelines = this.timelines[this.index:]
+		this.index = 0
+	}
+
+	return len(this.timelines) - this.index
 }
--- 933.Number_of_Recent_Calls/solution_test.go
+++ 933.Number_of_Recent_Calls/solution_test.go
@@ -14,27 +14,24 @@ func TestRecentCounter_Ping(t *testing.T) {
 		ts []int
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   []int
+		name string
+		args args
+		want []int
 	}{
 		{
 			"case1",
-			fields{0, 0, 0, make([]int, 3000), make([]int, 3000)},
 			args{[]int{1, 100, 3001, 3002}},
 			[]int{1, 2, 3, 3},
+		},
+		{
+			"case2",
+			args{[]int{642, 1849, 4921, 5936, 5957}},
+			[]int{1, 2, 1, 2, 3},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			this := &RecentCounter{
-				head:      tt.fields.head,
-				tail:      tt.fields.tail,
-				lastest:   tt.fields.lastest,
-				pings:     tt.fields.pings,
-				timelines: tt.fields.timelines,
-			}
+			this := Constructor()
 			for i, time := range tt.args.ts {
 				if got := this.Ping(time); got != tt.want[i] {
 					t.Errorf("RecentCounter.Ping() = %d, want %d", got, tt.want[i])
