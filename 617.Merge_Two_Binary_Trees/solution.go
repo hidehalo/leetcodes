d0667224d17f068fbdf6083cdf225e777af5afb8
hidehalo <tianchen_cc@yeah.net>
2020-04-22 10:39:39 +0800

    Update: LC617 impl(solved)

--- 617.Merge_Two_Binary_Trees/solution.go
+++ 617.Merge_Two_Binary_Trees/solution.go
@@ -1,19 +1,24 @@
 package main
 
 type TreeNode struct {
-	Val int
-	Left *TreeNode
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
- }
+}
 
 func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
-	if t1 == nil && t2 != nil {
+	if t1 == nil && t2 == nil {
+		return nil
+	} else if t1 == nil && t2 != nil {
 		return t2
 	} else if t2 == nil && t1 != nil {
 		return t1
-	} else {
-		
 	}
-	mergeTrees(t1.Left, t2.Left)
-	mergeTrees(t1.Right,t2.Right)
+
+	root := &TreeNode{}
+	root.Val = t1.Val + t2.Val
+	root.Left = mergeTrees(t1.Left, t2.Left)
+	root.Right = mergeTrees(t1.Right, t2.Right)
+
+	return root
 }
