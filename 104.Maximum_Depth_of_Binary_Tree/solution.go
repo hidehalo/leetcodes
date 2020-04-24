42357de50cdb4a211709e236d3ee64c3375b548e
hidehalo <tianchen_cc@yeah.net>
2020-04-24 10:47:05 +0800

    Update: LC104 impl(solved)

--- 104.Maximum_Depth_of_Binary_Tree/solution.go
+++ 104.Maximum_Depth_of_Binary_Tree/solution.go
@@ -1,5 +1,19 @@
 package main
 
 func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	} else if root.Left != nil && root.Right != nil {
+		return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+	} else if root.Right != nil {
+		return 1 + maxDepth(root.Right)
+	}
+	return 1 + maxDepth(root.Left)
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
