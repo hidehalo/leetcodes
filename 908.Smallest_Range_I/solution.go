c19836409e581ca3408a80806df1239acb0922e3
hidehalo <tianchen_cc@yeah.net>
2020-04-24 14:35:56 +0800

    Update: LC908 impl(solved)

--- 908.Smallest_Range_I/solution.go
+++ 908.Smallest_Range_I/solution.go
@@ -1,5 +1,18 @@
 package main
 
 func smallestRangeI(A []int, K int) int {
-
+	min, max := A[0], A[0]
+	for i := 0; i < len(A); i++ {
+		if A[i] < min {
+			min = A[i]
+		}
+		if A[i] > max {
+			max = A[i]
+		}
+	}
+	ret := max - min - 2*K
+	if ret > 0 {
+		return ret
+	}
+	return 0
 }
