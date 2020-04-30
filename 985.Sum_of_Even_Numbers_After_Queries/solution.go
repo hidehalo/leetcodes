a4b06834f8ee8a8828dde038da05f3c7a45713a5
hidehalo <tianchen_cc@yeah.net>
2020-04-30 17:31:18 +0800

    Update: LC985 impl(improve performance)

--- 985.Sum_of_Even_Numbers_After_Queries/solution.go
+++ 985.Sum_of_Even_Numbers_After_Queries/solution.go
@@ -2,16 +2,23 @@ package main
 
 func sumEvenAfterQueries(A []int, queries [][]int) []int {
 	ret := make([]int, 0)
+	evenSum := 0
+	for k := 0; k < len(A); k++ {
+		if A[k]%2 == 0 {
+			evenSum += A[k]
+		}
+	}
 	for _, q := range queries {
 		v := q[0]
 		i := q[1]
-		A[i] += v
-		evenSum := 0
-		for k := 0; k < len(A); k++ {
-			if A[k]%2 == 0 {
-				evenSum += A[k]
-			}
+		if A[i]%2 == 0 && (A[i]+v)%2 == 0 {
+			evenSum += v
+		} else if A[i]%2 != 0 && (A[i]+v)%2 == 0 {
+			evenSum += (A[i] + v)
+		} else if A[i]%2 == 0 && (A[i]+v)%2 != 0 {
+			evenSum -= A[i]
 		}
+		A[i] += v
 		ret = append(ret, evenSum)
 	}
 	return ret
