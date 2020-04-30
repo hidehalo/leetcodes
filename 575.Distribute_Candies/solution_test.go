eea07f6298a9370406b5098da818125740f5e1de
hidehalo <tianchen_cc@yeah.net>
2020-04-30 18:18:41 +0800

    Add: LC575 impl&test(unsolve)

---
+++ 575.Distribute_Candies/solution_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Test_distributeCandies(t *testing.T) {
+	type args struct {
+		candies []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"case1",
+			args{[]int{1, 1, 2, 2, 3, 3}},
+			3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := distributeCandies(tt.args.candies); got != tt.want {
+				t.Errorf("distributeCandies() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
