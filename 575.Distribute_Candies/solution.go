447a5f8c8b7819b14ac18b94a52a81889b4409f9
hidehalo <tianchen_cc@yeah.net>
2020-05-05 12:38:49 +0800

    Update: LC575 impl&test(solved)

--- 575.Distribute_Candies/solution.go
+++ 575.Distribute_Candies/solution.go
@@ -1,54 +1,20 @@
 package main
 
-import (
-	"sort"
-)
-
-type Candy struct {
-	key int
-	num int
-}
-
-type CandySlice struct {
-	data []*Candy
-}
-
-func (cs *CandySlice) Len() int {
-	return len(cs.data)
-}
-
-func (cs *CandySlice) Less(i, j int) bool {
-	return cs.data[i].num < cs.data[j].num
-}
-
-func (cs *CandySlice) Swap(i, j int) {
-	cs.data[i], cs.data[j] = cs.data[j], cs.data[i]
-}
-
 func distributeCandies(candies []int) int {
 	// greedy algo
-	// sort kind key asc order by kind's count
-	// plus candies of kind by order, check is sister's candies greater than brother's
-	// yes: return
-	// no: loop go on
-	counts := make(map[int]int)
+	// make a count for every kind of candies
+	// sis take one from every kind
+	// test sis's candies number less equal bro's
+	// yes: return number of kind
+	// no: return n(sis) - (n(sis) - n(bro) / 2)
+	counts := make(map[int]int) // hash table is too slow on big case, intger array is better
 	for i := 0; i < len(candies); i++ {
 		counts[candies[i]]++
 	}
-	candyList := make([]*Candy, 0)
-	for k, v := range counts {
-		candyList = append(candyList, &Candy{k, v})
+	countOfSis := len(counts)
+	countOfBro := len(candies) - len(counts)
+	if countOfSis <= countOfBro {
+		return countOfSis
 	}
-	sort.Sort(&CandySlice{candyList})
-	kindOfSis := 0
-	sumOfSis := 0
-	for i := 0; i < len(candyList); i++ {
-		kindOfSis++
-		sumOfSis += candyList[i].num
-		if sumOfSis >= len(candies)-sumOfSis {
-			return kindOfSis
-		}
-	}
-
-	return kindOfSis
+	return countOfSis - (countOfSis-countOfBro)/2
 }
--- 575.Distribute_Candies/solution_test.go
+++ 575.Distribute_Candies/solution_test.go
@@ -16,6 +16,11 @@ func Test_distributeCandies(t *testing.T) {
 			args{[]int{1, 1, 2, 2, 3, 3}},
 			3,
 		},
+		{
+			"case2",
+			args{[]int{1, 1, 2, 3}},
+			2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
