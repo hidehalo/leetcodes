ed5bf92fbb327229db6d72c3a50975c0a78d4814
hidehalo <tianchen_cc@yeah.net>
2020-05-05 23:05:54 +0800

    Update: LC1046 impl&test(solved)

--- 1046.Last_Stone_Weight/solution.go
+++ 1046.Last_Stone_Weight/solution.go
@@ -1,26 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
-type key int
-type pri int
-
 const (
-	Priority_EQ pri = 0
-	Priority_HI pri = 1
-	Priority_LO pri = 2
+	PriorityEQ = 0
+	PriorityHI = 1
+	PriorityLO = 2
 )
 
 type IPriority interface {
-	Compare(a int, b int) pri
+	Compare(a int, b int) int
 }
 
 type Heap struct {
 	priority IPriority
-	size     uint
+	size     int
 	store    []int
 }
 
@@ -29,25 +21,30 @@ func (h *Heap) IsEmpty() bool {
 }
 
 func (h *Heap) Top() int {
+	if h.size == 0 {
+		return -1
+	}
 	return h.store[0]
 }
 
-func (h *Heap) Insert(v int) {
-	if int(h.size) >= len(h.store) {
+func (h *Heap) Insert(v int) int {
+	if h.size >= len(h.store) {
 		h.store = append(h.store, v)
 	} else {
 		h.store[h.size] = v
 	}
-	i := key(h.size)
+	i := h.size
 	h.size++
 	for i > 0 {
-		isLow := h.priority.Compare(h.store[h.parent(i)], h.store[i]) == Priority_LO
+		isLow := h.priority.Compare(h.store[h.parent(i)], h.store[i]) == PriorityLO
 		if !isLow {
 			break
 		}
 		h.swap(h.parent(i), i)
 		i = h.parent(i)
 	}
+
+	return i
 }
 
 func (h *Heap) ExtractTop() int {
@@ -62,39 +59,26 @@ func (h *Heap) ExtractTop() int {
 	return top
 }
 
-func (h *Heap) String() string {
-	strBuf := make([]byte, 0)
-	for h.IsEmpty() != true {
-		v := h.ExtractTop()
-		strBuf = append(strBuf, []byte(strconv.FormatInt(int64(v), 10))...)
-		if h.IsEmpty() != true {
-			strBuf = append(strBuf, ' ')
-		}
-	}
-
-	return string(strBuf)
-}
-
-func (h *Heap) heapify(hi key) {
-	if hi >= key(h.size-1) {
+func (h *Heap) heapify(hi int) {
+	if hi >= h.size-1 {
 		return
 	}
 	l := h.left(hi)
 	r := h.right(hi)
-	_hi := hi
-	if h.priority.Compare(h.store[hi], h.store[l]) == Priority_LO {
-		_hi = l
+	newHi := hi
+	if l < h.size && h.priority.Compare(h.store[newHi], h.store[l]) == PriorityLO {
+		newHi = l
 	}
-	if h.priority.Compare(h.store[hi], h.store[r]) == Priority_LO {
-		_hi = r
+	if r < h.size && h.priority.Compare(h.store[newHi], h.store[r]) == PriorityLO {
+		newHi = r
 	}
-	if _hi != hi {
-		h.swap(_hi, hi)
-		h.heapify(hi)
+	if newHi != hi {
+		h.swap(newHi, hi)
+		h.heapify(newHi)
 	}
 }
 
-func (h *Heap) parent(i key) key {
+func (h *Heap) parent(i int) int {
 	p := (i - 1) >> 1
 	if p >= 0 {
 		return p
@@ -103,51 +87,48 @@ func (h *Heap) parent(i key) key {
 	return 0
 }
 
-func (h *Heap) left(i key) key {
-	return i<<1 + 1
+func (h *Heap) left(i int) int {
+	return h.right(i) - 1
 }
 
-func (h *Heap) right(i key) key {
-	return h.left(i) + 1
+func (h *Heap) right(i int) int {
+	return (i + 1) << 1
 }
 
-func (h *Heap) swap(a key, b key) {
+func (h *Heap) swap(a int, b int) {
 	tmp := h.store[a]
 	h.store[a] = h.store[b]
 	h.store[b] = tmp
 }
 
-type MaxHeap struct {
-	IPriority
-}
-
-func (h *MaxHeap) Compare(a int, b int) pri {
-	if a > b {
-		return Priority_HI
-	} else if a == b {
-		return Priority_EQ
-	}
-
-	return Priority_LO
-}
-
 func NewHeap(vals []int, priority IPriority) *Heap {
 	h := &Heap{
 		priority: priority,
-		size:     0,
-		store:    make([]int, 0, 1000),
+		size:     len(vals),
+		store:    make([]int, int(len(vals))),
 	}
-	for _, v := range vals {
-		h.Insert(v)
+	copy(h.store, vals)
+	for i := h.parent(h.size - 1); i >= 0; i-- {
+		h.heapify(i)
 	}
 
 	return h
 }
 
-// FIXME: max heap is bugy
+type MaxHeap struct{ IPriority }
+
+func (h *MaxHeap) Compare(a int, b int) int {
+	if a > b {
+		return PriorityHI
+	} else if a == b {
+		return PriorityEQ
+	}
+
+	return PriorityLO
+}
+
 func lastStoneWeight(stones []int) int {
 	maxHeap := NewHeap(stones, new(MaxHeap))
-	fmt.Println(maxHeap.store)
 
 	for maxHeap.IsEmpty() == false {
 		h1 := maxHeap.ExtractTop()
--- 1046.Last_Stone_Weight/solution_test.go
+++ 1046.Last_Stone_Weight/solution_test.go
@@ -11,36 +11,36 @@ func Test_lastStoneWeight(t *testing.T) {
 		args args
 		want int
 	}{
-		// {
-		// 	"case1",
-		// 	args{[]int{2, 7, 4, 1, 8, 1}},
-		// 	1,
-		// },
-		// {
-		// 	"case2",
-		// 	args{[]int{1, 2, 2}},
-		// 	1,
-		// },
-		// {
-		// 	"case3",
-		// 	args{[]int{1, 3}},
-		// 	2,
-		// },
-		// {
-		// 	"case4",
-		// 	args{[]int{1}},
-		// 	1,
-		// },
-		// {
-		// 	"case5",
-		// 	args{[]int{}},
-		// 	0,
-		// },
-		// {
-		// 	"case6",
-		// 	args{[]int{1, 1}},
-		// 	0,
-		// },
+		{
+			"case1",
+			args{[]int{2, 7, 4, 1, 8, 1}},
+			1,
+		},
+		{
+			"case2",
+			args{[]int{1, 2, 2}},
+			1,
+		},
+		{
+			"case3",
+			args{[]int{1, 3}},
+			2,
+		},
+		{
+			"case4",
+			args{[]int{1}},
+			1,
+		},
+		{
+			"case5",
+			args{[]int{}},
+			0,
+		},
+		{
+			"case6",
+			args{[]int{1, 1}},
+			0,
+		},
 		{
 			"case7",
 			args{[]int{10, 5, 4, 10, 3, 1, 7, 8}},
