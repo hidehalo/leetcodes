895ab2fc5d86ee0386df76e4b811d7b3bc74e564
hidehalo <tianchen_cc@yeah.net>
2020-05-09 21:39:52 +0800

    Update: LC566 impl&test(solved)

--- 566.Reshape_the_Matrix/solution.go
+++ 566.Reshape_the_Matrix/solution.go
@@ -1,5 +1,24 @@
 package main
 
 func matrixReshape(nums [][]int, r int, c int) [][]int {
+	if len(nums) == 0 {
+		return nums
+	}
+	total := len(nums) * len(nums[0])
+	if r*c > total || total/r > c || total/c > r {
+		return nums
+	}
+	ret := make([][]int, r)
+	ri, ci := 0, 0
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums[i]); j++ {
+			ret[ri] = append(ret[ri], nums[i][j])
+			ci = (ci + 1) % c
+			if ci == 0 {
+				ri++
+			}
+		}
+	}
 
+	return ret
 }
---
+++ 566.Reshape_the_Matrix/solution_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_matrixReshape(t *testing.T) {
+	type args struct {
+		nums [][]int
+		r    int
+		c    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"case1",
+			args{[][]int{{1, 2}, {3, 4}}, 1, 4},
+			[][]int{{1, 2, 3, 4}},
+		},
+		{
+			"case2",
+			args{[][]int{{1, 2}, {3, 4}}, 2, 4},
+			[][]int{{1, 2}, {3, 4}},
+		},
+		{
+			"case3",
+			args{[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}}, 42, 5},
+			[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := matrixReshape(tt.args.nums, tt.args.r, tt.args.c); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("matrixReshape() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
