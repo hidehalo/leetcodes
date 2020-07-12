3378391060ee287875c998ab9fc13b4b7db6ddd0
hidehalo <tianchen_cc@yeah.net>
2020-07-12 22:09:02 +0800

    unsolved: LC812

---
+++ 812.Largest_Triangle_Area/solution.go
@@ -0,0 +1,12 @@
+package main
+
+func largestTriangleArea(points [][]int) float64 {
+	minX,maxX,minY,maxY := 0,0,0,0
+    for i:=0 ;i<len(points);i++{
+		if i==0 {
+			minX,maxX,minY,maxY = points[0][0],points[0][0],points[0][1],points[0][1]
+		} else {
+
+		}
+	}
+}
---
+++ 812.Largest_Triangle_Area/solution_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Test_largestTriangleArea(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			"case1",
+			args{[][]int{{{0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}}},
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestTriangleArea(tt.args.points); got != tt.want {
+				t.Errorf("largestTriangleArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
