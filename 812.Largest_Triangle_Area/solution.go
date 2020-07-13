038379b35a8fb705b9a205bd1f3bf0aebc791b07
hidehalo <tianchen_cc@yeah.net>
2020-07-13 18:10:25 +0800

    unsolved: LC812

--- 812.Largest_Triangle_Area/solution.go
+++ 812.Largest_Triangle_Area/solution.go
@@ -1,12 +1,21 @@
 package main
 
 func largestTriangleArea(points [][]int) float64 {
-	minX,maxX,minY,maxY := 0,0,0,0
-    for i:=0 ;i<len(points);i++{
-		if i==0 {
-			minX,maxX,minY,maxY = points[0][0],points[0][0],points[0][1],points[0][1]
+	minX, maxX, minY, maxY := 0, 0, 0, 0
+	for i := 0; i < len(points); i++ {
+		if i == 0 {
+			minX, maxX, minY, maxY = points[0][0], points[0][0], points[0][1], points[0][1]
 		} else {
-
+			if points[i][0] < minX {
+				minX = points[i][0]
+			} else if points[i][0] > maxX {
+				maxX = points[i][0]
+			}
+			if points[i][1] < minY {
+				minY = points[i][1]
+			} else if points[i][1] > maxY {
+				maxY = points[i][1]
+			}
 		}
 	}
 }
