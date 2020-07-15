3a91053de43727a2c66f668fc7ff92d87109c295
hidehalo <tianchen_cc@yeah.net>
2020-07-15 18:46:42 +0800

    unsolve: LC111

---
+++ 111.Minimum_Depth_of_Binary_Tree/solution.go
@@ -0,0 +1,29 @@
+package main
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right == nil {
+
+	} else if root.Left == nil && root.Right != nil {
+
+	} else if root.Left != nil && root.Right == nil {
+
+	} else {
+
+	}
+}
+
+func countDepth(root *TreeNode, d int) int {
+	if root.Left == nil && root.Right == nil {
+		return d
+	} else if root.Left == nil && root.Right != nil {
+
+	} else if root.Left != nil && root.Right == nil {
+
+	} else {
+
+	}
+}
