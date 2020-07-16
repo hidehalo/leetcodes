09a143c214d37df31c80f63ff29651c00334824c
hidehalo <tianchen_cc@yeah.net>
2020-07-16 18:10:54 +0800

    solved: LC111 impl

--- 111.Minimum_Depth_of_Binary_Tree/solution.go
+++ 111.Minimum_Depth_of_Binary_Tree/solution.go
@@ -3,27 +3,20 @@ package main
 func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
-	}
-
-	if root.Left == nil && root.Right == nil {
-
+	} else if root.Left == nil && root.Right == nil {
+		return 1
 	} else if root.Left == nil && root.Right != nil {
-
+		return 1 + minDepth(root.Right)
 	} else if root.Left != nil && root.Right == nil {
-
-	} else {
-
+		return 1 + minDepth(root.Left)
 	}
+
+	return 1 + min(minDepth(root.Left), minDepth(root.Right))
 }
 
-func countDepth(root *TreeNode, d int) int {
-	if root.Left == nil && root.Right == nil {
-		return d
-	} else if root.Left == nil && root.Right != nil {
-		return countDepth()
-	} else if root.Left != nil && root.Right == nil {
-
-	} else {
-
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
+	return b
 }
