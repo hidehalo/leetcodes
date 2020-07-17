2c23f92a5378fac6be0d3a5da3094eee40fb66cc
hidehalo <tianchen_cc@yeah.net>
2020-07-17 16:20:04 +0800

    solved: LC108 impl

--- 108.Convert_Sorted_Array_to_Binary_Search_Tree/solution.go
+++ 108.Convert_Sorted_Array_to_Binary_Search_Tree/solution.go
@@ -1,5 +1,21 @@
 package main
 
 func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	root := &TreeNode{
+		Left:  nil,
+		Right: nil,
+		Val:   nums[mid],
+	}
+	if mid > 0 {
+		root.Left = sortedArrayToBST(nums[:mid])
+	}
+	if mid+1 <= len(nums) {
+		root.Right = sortedArrayToBST(nums[mid+1:])
+	}
 
+	return root
 }
