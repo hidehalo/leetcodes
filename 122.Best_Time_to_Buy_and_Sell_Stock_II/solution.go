f9f4f880896fe6addbf020cb20dcd71149605f8f
hidehalo <tianchen_cc@yeah.net>
2020-07-21 15:52:06 +0800

    solved: LC122 impl

--- 122.Best_Time_to_Buy_and_Sell_Stock_II/solution.go
+++ 122.Best_Time_to_Buy_and_Sell_Stock_II/solution.go
@@ -4,5 +4,31 @@ func maxProfit(prices []int) int {
 	if len(prices) == 0 {
 		return 0
 	}
-	return 0
+	buy := prices[0]
+	profit := 0
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		buy = min(buy, prices[i])
+		profit = max(profit, prices[i]-buy)
+		if prices[i] < prices[i-1] {
+			maxProfit += profit
+			profit = 0
+			buy = prices[i]
+		}
+	}
+	return maxProfit + profit
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
 }
