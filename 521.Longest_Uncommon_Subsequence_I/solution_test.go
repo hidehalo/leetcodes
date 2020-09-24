ef4fb00bb9aec915381f03577f65044e20eabf20
hidehalo <tianchen_cc@yeah.net>
2020-09-24 16:42:32 +0800

    solved: LC521 impl&test

---
+++ 521.Longest_Uncommon_Subsequence_I/solution_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func Test_findLUSlength(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"case1",
+			args{"aba", "cdc"},
+			3,
+		},
+		{
+			"case2",
+			args{"aaa", "bbb"},
+			3,
+		},
+		{
+			"case3",
+			args{"aaa", "aaa"},
+			-1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findLUSlength(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("findLUSlength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
